c11dfd37d29da45076b6022e53c8ad92802b30ae
yukavio <kavioyu@gmail.com>
2020-11-30 12:31:43 +0000

    temp

--- lite/backends/arm/math/conv_impl.cc
+++ lite/backends/arm/math/conv_impl.cc
@@ -125,21 +125,31 @@ void im2col(const Dtype* data_im,
           stride_w +
       1;
   const int channel_size = height * width;
-  for (int channel = channels; channel--; data_im += channel_size) {
-    for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) {
-      for (int kernel_col = 0; kernel_col < kernel_w; kernel_col++) {
+  for (int channel = channels; channel--; data_im += channel_size) 
+  {
+    for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) 
+    {
+      for (int kernel_col = 0; kernel_col < kernel_w; kernel_col++) 
+      {
         int input_row = -pad_top + kernel_row * dilation_h;
-        for (int output_rows = output_h; output_rows; output_rows--) {
-          if (!is_a_ge_zero_and_a_lt_b(input_row, height)) {
+        for (int output_rows = output_h; output_rows; output_rows--) 
+        {
+          if (!is_a_ge_zero_and_a_lt_b(input_row, height)) 
+          {
             for (int output_cols = output_w; output_cols; output_cols--) {
               *(data_col++) = 0;
             }
-          } else {
+          } 
+          else 
+          {
             int input_col = -pad_left + kernel_col * dilation_w;
-            for (int output_col = output_w; output_col; output_col--) {
+            for (int output_col = output_w; output_col; output_col--) 
+            {
               if (is_a_ge_zero_and_a_lt_b(input_col, width)) {
                 *(data_col++) = data_im[input_row * width + input_col];
-              } else {
+              } 
+              else 
+              {
                 *(data_col++) = 0;
               }
               input_col += stride_w;
